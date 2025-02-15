159305681e0131989b6600d369d14ee412e059dc
valya <valekmalek0206@gmail.com>
2025-02-15 11:37:05 +0300

    изменения

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,8 @@ import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
     private Integer count = 0;
+    private TextView txtCounter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +29,75 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        txtCounter = findViewById(R.id.txtCounter);
+        updateCounterText();
     }
 
-    public void onClickBatton(View view) {
+    public void onClickButtonIncrement(View view) {
         count++;
-        TextView txt= findViewById(R.id.txtCounter);
-        txt.setText(count.toString());
+        updateCounterText();
+    }
+
+    public void onClickButtonDecrement(View view) {
+        count--;
+        updateCounterText();
+    }
+
+    private void updateCounterText() {
+        txtCounter.setText(count.toString());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Instens", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Instens", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Instens", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Instens", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Instens", "onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle autState) {
+        super.onSaveInstanceState(autState);
+        autState.putInt("currentCount", count);
+        Log.d("Instens", "onSave");
+        Toast.makeText(this, "onSave", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle SavedState) {
+        super.onRestoreInstanceState(SavedState);
+        if (SavedState != null && SavedState.containsKey("currentCount")) {
+            count = SavedState.getInt("currentCount");
+            updateCounterText();
+        }
+        Log.d("Instens", "get");
+        Toast.makeText(this, "get", Toast.LENGTH_LONG).show();
     }
 }
